050df12e42e15d8db8443ddb2dff557c99b6e1a5
pajouheshgar <pajouheshgar@ce.sharif.edu>
2019-06-01 17:43:57 +0430

    .

--- fuck.py
+++ fuck.py
@@ -1,5 +1,5 @@
 dickt = {}
-with open(".csv", encoding=) as f:
+with open(".csv", encoding='utf-8') as f:
     for a in f.readlines():
         key = a[:a.find("\t")]
         value = a[a.find("\t") + 1:]
